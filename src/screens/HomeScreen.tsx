f4a68813353e24e77b3764ec61dc52b9c2847a01
Johny Villegas <johnyvgoog@gmail.com>
2022-03-06 10:03:40 -0500

    Implementando react-query

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { CountryCard } from '../components/CountryCard';
 import { useCountry } from '../hooks/useCountry';
 
+import { useQuery } from 'react-query';
+import { ResponseAll } from '../interfaces/responseAll';
+
 export const HomeScreen = () => {
 
-  useCountry();
+  const { isLoading, error, data } = useQuery('all', () =>
+    fetch('https://restcountries.com/v3.1/all').then(res =>
+      res.json()
+    )
+  );
+
+  console.log('DATA: ', data);
+  
 
   return (
     <View style={StylesHome.bodyHome}>
@@ -15,7 +25,7 @@ export const HomeScreen = () => {
 }
 
 const StylesHome = StyleSheet.create({
-    bodyHome: {
-      flex: 1
-    }
+  bodyHome: {
+    flex: 1
+  }
 });
