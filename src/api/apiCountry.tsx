765bb85b0139bb178718a50e5caad26686579ec7
Johny Villegas <johnyvgoog@gmail.com>
2022-03-07 18:43:19 -0500

    Se añade el selector de regiones

--- src/api/apiCountry.tsx
+++ src/api/apiCountry.tsx
@@ -4,6 +4,8 @@ import { CountryBasic } from '../interfaces/countryBasic';
 
 const ApiCountry = axios.create({baseURL: 'https://restcountries.com/v3.1'});
 
+export const Regions: string[] = ['Asia', 'Africa', 'Americas', 'Europe'];
+
 
 const getAll = async () => {
     const resp = await ApiCountry.get<ResponseAll[]>('/all');
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,33 +1,50 @@
-import React, { useEffect } from 'react';
-import { Text, View, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
 import { CountryCard } from '../components/CountryCard';
 import { useQuery } from 'react-query';
-import { getAll } from '../api/apiCountry';
+import { getAll, Regions } from '../api/apiCountry';
+import { Picker } from '@react-native-picker/picker';
 
 export const HomeScreen = () => {
 
   const { isLoading, error, data } = useQuery('all', getAll);
-  
+
+  const [selectedRegion, setselectedRegion] = useState();
+
   return (
     <View style={StylesHome.bodyHome}>
+      <View style={StylesHome.viewPicker}>
+        <Picker
+          selectedValue={selectedRegion}
+          onValueChange={(itemValue, itemIndex) => setselectedRegion(itemValue)}
+        >
+          <Picker.Item enabled={false} label="Filter by Region" value="0" />
+          {
+            Regions.map(item => <Picker.Item key={item} label={item} value={item} />)
+          }
 
-      {
-        (isLoading)
-          ?(
-              <ActivityIndicator 
-                  style={StylesHome.activityIndicator}
-                  size={80}
-                  color="#171717"
+        </Picker>
+      </View>
+
+      <View>
+        {
+          (isLoading)
+            ? (
+              <ActivityIndicator
+                style={StylesHome.activityIndicator}
+                size={80}
+                color="#171717"
               />
-          ):(
-            <FlatList 
-              data={data}
-              keyExtractor={(item) => item.name.official}
-              showsVerticalScrollIndicator={false}
-              renderItem={({item}) => <CountryCard country={item} />}
-            />
-          )
-      }
+            ) : (
+              <FlatList
+                data={data}
+                keyExtractor={(item) => item.name.official}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => <CountryCard country={item} />}
+              />
+            )
+        }
+      </View>
 
     </View>
   )
@@ -39,5 +56,12 @@ const StylesHome = StyleSheet.create({
   },
   activityIndicator: {
     height: 400
+  },
+  viewPicker: {
+    backgroundColor: 'white',
+    marginVertical: 10,
+    marginLeft: 25,
+    width: 250,
+    borderRadius: 5
   }
 });
